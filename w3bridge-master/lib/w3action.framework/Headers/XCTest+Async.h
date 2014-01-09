3405abc69317b6994ac8d2b6b708f97767113fc5
KH Kim <hh963103@naver.com>
2014-01-09 19:15:32 +0900

    Refactoring

--- w3bridge-master/lib/w3action.framework/Headers/XCTest+Async.h
+++ w3bridge-master/lib/w3action.framework/Headers/XCTest+Async.h
@@ -1,9 +1,9 @@
 //
-//  MultipartFormDataObject.h
+//  XCTest+Async.h
 //  w3action
 //
-//  Created by KH Kim on 2013. 12. 30..
-//  Copyright (c) 2013 KH Kim. All rights reserved.
+//  Created by KH Kim on 2014. 1. 2..
+//  Copyright (c) 2014년 KH Kim. All rights reserved.
 //
 
 /*
@@ -22,10 +22,10 @@
  limitations under the License.
  */
 
-#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
 
-@interface MultipartFormDataObject : NSObject
-@property (nonatomic, retain) NSString *filename;
-@property (nonatomic, retain) NSData *data;
-+ (MultipartFormDataObject *)objectWithFilename:(NSString *)filename data:(NSData *)data;
+typedef void (^FinishBlock)(void);
+
+@interface XCTest (org_apache_w3action_XCTest)
+- (void)async:(void (^)(FinishBlock finish))execution;
 @end
